1af2e717fcf691e513fd882bc2dc1c03e1333a22
X512 <danger_mail@list.ru>
2023-01-05 06:38:51 +0900

    HandleTable: do not allow multiple handles to the same object

--- HandleTable.h
+++ HandleTable.h
@@ -7,6 +7,7 @@
 template <typename Type>
 class HandleTable {
 private:
+	std::map<void*, int32> fObjects;
 	std::map<int32, Type> fHandles;
 	ExternalAllocator fHandlePool;
 
@@ -14,7 +15,7 @@ public:
 	HandleTable();
 
 	int32 Register(Type obj);
-	status_t Free(int32 handle); 
+	status_t Free(int32 handle);
 	Type This(int32 handle);
 
 	inline std::map<int32, Type> &Handles() {return fHandles;}
@@ -30,9 +31,12 @@ template <typename Type>
 int32 HandleTable<Type>::Register(Type obj)
 {
 	//if (!obj.IsSet()) return B_ERROR;
+	auto it = fObjects.find(obj.ref.Get());
+	if (it != fObjects.end()) return it->second;
 	uint64_t handle64;
 	if (!fHandlePool.Alloc(handle64, 1)) return B_NO_MEMORY;
 	int32 handle = (int32)handle64;
+	fObjects.emplace(obj.ref.Get(), handle);
 	fHandles.emplace(handle, obj);
 	return handle;
 }
@@ -42,6 +46,7 @@ status_t HandleTable<Type>::Free(int32 handle)
 {
 	auto it = fHandles.find(handle);
 	if (it == fHandles.end()) return B_ERROR;
+	fObjects.erase(it->second.ref.Get());
 	fHandles.erase(it);
 	fHandlePool.Free(handle);
 	return B_OK;
